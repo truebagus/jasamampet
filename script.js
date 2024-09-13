84d9f09ed5aa923f797b4e875bfaea62bfeb1f2b
Tubagus Maulana <142872206+truebagus@users.noreply.github.com>
2024-09-13 13:19:39 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+const heroSection = document.querySelector('.hero-section');
+
+    const images = [
+        'url(https://www.southsurreyplumbing.com/wp-content/uploads/2020/03/drain-clogging-1.jpg)',
+        'url(https://www.emergencyplumbingsquad.com/wp-content/uploads/2021/01/plumber-after-unclogging-sink-drain-photo-768x512.jpg)',
+        'url(https://novinkhadamat.com/wp-content/uploads/2021/08/profesional-clogging-work-using-machine.jpg)',
+        'url(https://aceplumbingbaltimore.com/wp-content/uploads/2022/12/clogged-toilet.jpg)'
+    ];
+
+    let currentImageIndex = 0;
+
+    function changeBackgroundImage() {
+        heroSection.style.backgroundImage = images[currentImageIndex];
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+    }
+
+    // Change background every 5 seconds
+    setInterval(changeBackgroundImage, 5000);
+
+    // Initial background setup
+    changeBackgroundImage();
+
+const serviceCards = document.querySelector('.service-cards');
+const cards = document.querySelectorAll('.card');
+const cardWidth = cards[0].offsetWidth + 20; // Width of a card + gap
+const totalScrollWidth = cardWidth * cards.length;
+let scrollAmount = 0;
+
+function autoScroll() {
+    scrollAmount += cardWidth;
+    if (scrollAmount >= totalScrollWidth) {
+        scrollAmount = 0; // Kembali ke awal saat mencapai akhir
+    }
+    serviceCards.scrollTo({
+        left: scrollAmount,
+        behavior: "smooth"
+    });
+}
+
+// Set interval untuk menggulir otomatis setiap 5 detik
+setInterval(autoScroll, 5000);
