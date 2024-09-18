6c61a91bb4d12783a6eb0ed243e1c6b507918d99
Tubagus Maulana <iniguschannel@gmail.com>
2024-09-18 11:08:30 +0700

    Update hero.js

--- js/hero.js
+++ js/hero.js
@@ -3,7 +3,7 @@ const hero = document.querySelector('.hero');
 // Array of background image URLs
 const images = [
     'https://th.bing.com/th/id/OIG4.A7D4QS7TtJAUuSJ8igKU?w=1024&h=1024&rs=1&pid=ImgDetMain',
-    'https://th.bing.com/th/id/OIP.0vNYQG1D1H6K2easQuDMEAHaEy?rs=1&pid=ImgDetMain',
+    'https://cdn.homedit.com/wp-content/uploads/2021/06/Plumbing-pipe-installation.jpg',
     'https://procrewschedule.com/wp-content/uploads/2020/06/plumber-at-work-in-a-bathroom-1024x683-1.jpg',
     'https://th.bing.com/th/id/OIG4.9I0nAy.EK4lSD1RWX5vW?w=1024&h=1024&rs=1&pid=ImgDetMain'
 ];
